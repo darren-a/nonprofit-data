95ac876ad229e31230a418dfa5c8b84e34133070
darren-a <darren_adkinson@hotmail.com>
2017-10-28 22:07:12 -0700

    Starting to get CORS running even though API doesn not support it. Using cors-anywhere.

---
+++ np_data.js
@@ -0,0 +1,141 @@
+
+// cors-anywhere
+// https://github.com/Rob--W/cors-anywhere/#documentation
+// If you want to automatically enable cross-domain 
+// requests when needed, use the following snippet:
+
+(function() {
+    var cors_api_host = 'cors-anywhere.herokuapp.com';
+    var cors_api_url = 'https://' + cors_api_host + '/';
+    var slice = [].slice;
+    var origin = window.location.protocol + '//' + window.location.host;
+    var open = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function() {
+        var args = slice.call(arguments);
+        var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
+        if (targetOrigin && targetOrigin[0].toLowerCase() !== origin &&
+            targetOrigin[1] !== cors_api_host) {
+            args[1] = cors_api_url + args[1];
+        }
+        return open.apply(this, args);
+    };
+})();
+
+
+
+const URL = 'https://projects.propublica.org/nonprofits/api/v2/';
+const BASE_SEARCH = URL + 'search.json?q=';
+const CA_SEARCH = BASE_SEARCH + 'state%5BCA%5D';
+const TEST_URL = 'https://httpbin.org/';
+
+
+
+var handlers = {
+
+  getCAData : function() {
+
+  	view.displayCAData();
+  }
+
+}
+
+
+
+
+
+var view = {
+
+  displayCAData : function() {
+
+  	var url = CA_SEARCH;
+  	//var url = TEST_URL;
+
+  	makeCorsRequest(url);
+
+    console.log('cors req has been sent');
+  }
+
+
+}
+  
+
+
+
+  /*
+  setupEventListeners: function() {
+    
+    //var todosUl = document.querySelector("ul");
+
+    todosUl.addEventListener('click', function(event) { 
+      console.log(event.target.parentNode.id);
+      // get the element that was clicked on
+      var elementClicked = event.target;
+    });
+    
+  }
+  */
+
+
+
+//view.setupEventListeneres();
+
+
+
+
+/**
+took this all out as I'm pretty sure Pro Publica doesn't support CORS
+**/
+
+// https://www.html5rocks.com/en/tutorials/cors/
+// Create the XHR object.
+function createCORSRequest(method, url) {
+
+  var xhr = new XMLHttpRequest();
+  
+  if("withCredentials" in xhr) {
+    // XHR for Chrome/Firefox/Opera/Safari.
+    xhr.open(method, url, true);
+  } else if(typeof XDomainRequest !== "undefined") {
+    // XDomainRequest for IE.
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+  } else {
+    // CORS not supported.
+    xhr = null;
+  }
+  return xhr;
+}
+
+
+// Helper method to parse the title tag from the response.
+function getTitle(text) {
+  return text.match('<title>(.*)?</title>')[1];
+}
+
+
+// Make the actual CORS request.
+function makeCorsRequest(url) {
+  // This is a sample server that supports CORS.
+  //var url = 'http://html5rocks-cors.s3-website-us-east-1.amazonaws.com/index.html';
+
+  var xhr = createCORSRequest('GET', url);
+  if (!xhr) {
+    alert('CORS not supported');
+    return;
+  }
+
+  // Response handlers.
+  xhr.onload = function() {
+    var text = xhr.responseText;
+    var title = getTitle(text);
+    alert('Response from CORS request to ' + url + ': ' + title);
+  };
+
+  xhr.onerror = function() {
+    alert('Woops, there was an error making the request.');
+  };
+
+  xhr.send();
+}
+
+
